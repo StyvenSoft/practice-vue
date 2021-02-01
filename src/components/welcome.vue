0fdd14a67acdc96df57dfaa3fa7054770bedf735
Steveen <steveen.echeverri@gmail.com>
2021-02-01 11:50:29 -0500

    Creating Vue expressions

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -2,25 +2,42 @@
   <h1>Welcome</h1>
   <p>The static HTML</p>
   <p>My name is {{ myName }} i'm my profile is {{ profile }}</p>
+  <p>result sum: {{ 5 + 6 }}</p>
+  <p>Conditional {{ 1 == 2 ? 'Correct' : 'Error'}} 
+      {{ Math.random() > 0.5 ? 'True' : 'False'}}
+      {{ Math.sqrt(100) }}
+  </p>
 </template>
 
 <script>
-const something = {
+
+// function somethingSum (a, b) {
+
+// }
+
+// somethingSum(10 > 20 ? 30 : 40, 20)
+// somethingSum([1,2,3].map(t => t ** 2), 20)
+
+
+export default {
   data() {
     return {
       myName: "Steveen Echeverri",
       profile: "Developer",
     };
   },
+  methods: {
+      messageAlert() {
+          alert("New message enabled");
+      },
+  },
   mounted() {
-      this.myName = "Undefined"
-      console.log(this.myName); // Undefined
+    //   this.myName = "Undefined"
+    //   console.log(this.myName); // Undefined
+
+    this.messageAlert();
   },
 };
-
-something.mounted() // This works in javascript
-
-export default something
 </script>
 
 <style scoped>
