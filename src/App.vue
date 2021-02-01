05edc069349deacf7446100e06d068935a69ad37
Steveen <steveen.echeverri@gmail.com>
2021-02-01 11:08:01 -0500

    Created new component with data

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
+<!-- View section -->
 <template>
+  <Welcome />
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Hello world friends!"/>
 </template>
 
+<!-- Logic section -->
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Welcome from './components/welcome.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Welcome
   }
 }
 </script>
 
+<!-- Styles section -->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/welcome.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Welcome</h1>
+    <p>The static HTML</p>
+    <p>My name is {{ myName }} i'm my profile is {{ profile }}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            myName: "Steveen Echeverri",
+            profile: "Developer"
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
