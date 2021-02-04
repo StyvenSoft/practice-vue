77634975aecba7745f11bbc7c18f1cd6fb91fa08
Steveen <steveen.echeverri@gmail.com>
2021-02-04 11:59:00 -0500

    Completed exercise speedTyper

--- src/components/speedTyper.vue
+++ src/components/speedTyper.vue
@@ -1,19 +1,21 @@
 <template>
     <h1>Speed Typer</h1>
+    <p>Your score: 
+        {{ keywords.filter(keyword => keyword.correct).length }}
+        / {{ keywords.length }}</p>
     <p>
-        <span :key="keyword" v-for="keyword in keywords">
-            {{ keyword }} {{ ' ' }}
+        <span 
+            :class="{ correct: keyword.correct, wrong: keyword.wrong, pending: keyword.pending }"
+            :key="keyword.text" v-for="keyword in keywords">
+            {{ keyword.text }} {{ ' ' }}
         </span>
     </p>
     <input type="text" :value="inputValue" @keyup.space="processInput($event)"> 
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            inputValue: "",
-            keywords: [
+
+const defaultKeywords = [
                 'template', 
                 'data', 
                 'javascript', 
@@ -26,7 +28,20 @@ export default {
                 'angular',
                 'nextjs',
                 'laravel'
-            ]
+            ].map(keywords => {
+                return {
+                    text: keywords,
+                    correct: false,
+                    wrong: false,
+                    pending: true,
+                }
+            })
+export default {
+    data() {
+        return {
+            inputValue: "",
+            index: 0,
+            keywords: defaultKeywords,
         };
     },
     methods: {
@@ -35,6 +50,18 @@ export default {
             if (value === "") {
                 return;
             }
+
+            if (this.keywords[this.index].text === value) {
+                this.keywords[this.index].correct = true
+                this.keywords[this.index].wrong = false
+                this.keywords[this.index].pending = false
+            } else {
+                this.keywords[this.index].correct = false
+                this.keywords[this.index].wrong = true
+                this.keywords[this.index].pending = false
+            }
+            this.inputValue = "";
+            this.index++;
         }
     },
 }
@@ -42,4 +69,19 @@ export default {
 
 
 <style scoped>
+
+.pending {
+    font-weight: bold;
+}
+
+.correct {
+    font-weight: bold;
+    color: green;
+}
+
+.wrong {
+    font-weight: bold;
+    color:red;
+}
+
 </style>
